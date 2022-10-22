647013cc6ca7030decaa88bd31ed6f0a6fe7832a
codyh <overbeck.quinn@pm.me>
2022-10-22 06:28:21 -0400

    rr / eocv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/e_vision/SimpleThresholdPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/e_vision/SimpleThresholdPipeline.java
@@ -1,99 +1,85 @@
 package org.firstinspires.ftc.teamcode;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleThresholdPipeline extends OpenCvPipeline {
 
-	/*
-	 * These are our variables that will be
-	 * modifiable from the variable tuner.
-	 *
-	 * Scalars in OpenCV are generally used to
-	 * represent color. So our values in the
-	 * lower and upper Scalars here represent
-	 * the Y, Cr and Cb values respectively.
-	 *
-	 * YCbCr, like most color spaces, range
-	 * from 0-255, so we default to those
-	 * min and max values here for now, meaning
-	 * that all pixels will be shown.
-	 */
-	public Scalar lower = new Scalar(0, 0, 0);
-	public Scalar upper = new Scalar(255, 255, 255);
 
-	/*
-	 * A good practice when typing EOCV pipelines is
-	 * declaring the Mats you will use here at the top
-	 * of your pipeline, to reuse the same buffers every
-	 * time. This removes the need to call mat.release()
-	 * with every Mat you create on the processFrame method,
-	 * and therefore, reducing the possibility of getting a
-	 * memory leak and causing the app to crash due to an
-	 * "Out of Memory" error.
-	 */
-	private Mat ycrcbMat = new Mat();
+	public Scalar low = new Scalar(31, 152, 95);
+	public Scalar high = new Scalar(162, 255, 128);
+
+
+/*	private Mat ycrcbMat = new Mat();
 	private Mat binaryMat = new Mat();
-	private Mat maskedInputMat = new Mat();
+	private Mat maskedInputMat = new Mat();*/
+
+	private Mat mask;
+	private Mat frame;
+	private Mat output;
+	private Point centroid;
 
 	@Override
 	public Mat processFrame(Mat input) {
-		/*
-		 * Converts our input mat from RGB to YCrCb.
-		 * EOCV ALWAYS returns RGB mats, so you'd
-		 * always convert from RGB to the color
-		 * space you want to use.
-		 *
-		 * Takes our "input" mat as an input, and outputs
-		 * to a separate Mat buffer "ycrcbMat"
-		 */
-		Imgproc.cvtColor(input, ycrcbMat, Imgproc.COLOR_RGB2YCrCb);
+		frame  = new Mat();
+		output = new Mat();
+		mask   = new Mat(output.rows(), output.cols(), CvType.CV_8UC4);
 
-		/*
-		 * This is where our thresholding actually happens.
-		 * Takes our "ycrcbMat" as input and outputs a "binary"
-		 * Mat to "binaryMat" of the same size as our input.
-		 * "Discards" all the pixels outside the bounds specified
-		 * by the scalars above (and modifiable with EOCV-Sim's
-		 * live variable tuner.)
-		 *
-		 * Binary meaning that we have either a 0 or 255 value
-		 * for every pixel.
-		 *
-		 * 0 represents our pixels that were outside the bounds
-		 * 255 represents our pixels that are inside the bounds
-		 */
-		Core.inRange(ycrcbMat, lower, upper, binaryMat);
+		// Converts the colorspace from RGB to YCrCb
+		Imgproc.cvtColor(input, output, Imgproc.COLOR_RGB2YCrCb);
+		// Creates a mask of all pixels within the specified color bounds
+		Core.inRange(output, low, high, mask);
+		// Puts the mask over the original image
+		Core.bitwise_and(input, input, frame, mask);
 
-		/*
-		 * Release the reusable Mat so that old data doesn't
-		 * affect the next step in the current processing
-		 */
-		maskedInputMat.release();
+		// Blurs the image to smooth it out and reduce unwanted pixels
+		Imgproc.GaussianBlur(mask, mask, new Size(11, 15), 0.0);
 
-		/*
-		 * Now, with our binary Mat, we perform a "bitwise and"
-		 * to our input image, meaning that we will perform a mask
-		 * which will include the pixels from our input Mat which
-		 * are "255" in our binary Mat (meaning that they're inside
-		 * the range) and will discard any other pixel outside the
-		 * range (RGB 0, 0, 0. All discarded pixels will be black)
-		 */
-		Core.bitwise_and(input, input, maskedInputMat, binaryMat);
+		// Initialize the Mat and ArrayList and finds the contours on the image
+		List<MatOfPoint> contours  = new ArrayList<>();
+		Mat              hierarchy = new Mat();
+		Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 
-		/*
-		 * The Mat returned from this method is the
-		 * one displayed on the viewport.
-		 *
-		 * To visualize our threshold, we'll return
-		 * the "masked input mat" which shows the
-		 * pixel from the input Mat that were inside
-		 * the threshold range.
-		 */
-		return maskedInputMat;
+		// Draws contours onto the frame from the list created above. During this it searches for
+		// the biggest contour and saves it to the variable biggest.
+		if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
+			MatOfPoint biggest = new MatOfPoint();
+			for (int index = 0; index >= 0; index = (int) hierarchy.get(0, index)[0]) {
+				Imgproc.drawContours(frame, contours, index, new Scalar(88, 0, 0), 2);
+				if (index == 0) {
+					biggest = contours.get(index);
+				} else if (contours.get(index).size().area() > contours.get(index - 1).size().area()) {
+					biggest = contours.get(index);
+				}
+			}
+
+			// Creates a point and sets it to the approximate center of the largest contour
+			Moments moments = Imgproc.moments(biggest);
+			centroid = new Point();
+
+			centroid.x = moments.get_m10() / moments.get_m00();
+			centroid.y = moments.get_m01() / moments.get_m00();
+
+			Rect rect = new Rect((int) centroid.x, (int) centroid.y, 10, 10);
+			Imgproc.rectangle(frame, rect, new Scalar(0, 255, 255));
+
+		}
+
+		mask.release();
+
+		return frame;
 	}
 
 }
