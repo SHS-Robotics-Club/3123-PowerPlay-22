bd41729ec9dbed81e3fc6dbf87b6551426fe677d
codyh <neochaotics@pm.me>
2023-01-22 12:49:14 -0500

    pid testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/MainTeleOp.java
@@ -13,11 +13,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.b_commands.DriveCommand;
 import org.firstinspires.ftc.teamcode.c_subsystems.ClawSubsystem;
 import org.firstinspires.ftc.teamcode.c_subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.c_subsystems.LiftSubsystemOther;
 
 //@Disabled
 @Config
 @TeleOp(name = "MainTeleOp", group = ".")
 public class MainTeleOp extends CommandOpMode {
+	public static final double target = 300;
 	@Override
 	public void initialize() {
 		if (isStopRequested()) return; // Exit program if stop requested??
@@ -29,9 +31,9 @@ public class MainTeleOp extends CommandOpMode {
 		GamepadEx gPad1 = new GamepadEx(gamepad1);
 
 		// Define Systems ----------------------------------------------------------------------------------------------------
-		DriveSubsystem driveSubsystem = new DriveSubsystem(devices.frontLeft, devices.frontRight, devices.backLeft, devices.backRight);
-		ClawSubsystem  claw           = new ClawSubsystem(devices.clawLeft, devices.clawRight);
-
+		DriveSubsystem     driveSubsystem = new DriveSubsystem(devices.frontLeft, devices.frontRight, devices.backLeft, devices.backRight);
+		ClawSubsystem      claw           = new ClawSubsystem(devices.clawLeft, devices.clawRight);
+		LiftSubsystemOther lfo            = new LiftSubsystemOther(devices.lift);
 
 		DriveCommand driveCommand = new DriveCommand(driveSubsystem, gPad1::getLeftX, gPad1::getLeftY, gPad1::getRightX, 1);
 
@@ -46,6 +48,12 @@ public class MainTeleOp extends CommandOpMode {
 					     return claw.isOpen();
 				     }
 		     ));
+
+		if (gPad1.getButton(GamepadKeys.Button.DPAD_UP)) {
+			lfo.set(target);
+		} else if (gPad1.getButton(GamepadKeys.Button.DPAD_DOWN)) {
+			lfo.stop();
+		}
 
 		// Register and Schedule ----------------------------------------------------------------------------------------------------
 		register(driveSubsystem);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/c_subsystems/LiftSubsystemOther.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/c_subsystems/LiftSubsystemOther.java
@@ -4,46 +4,47 @@ import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.arcrobotics.ftclib.controller.wpilibcontroller.ElevatorFeedforward;
-import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
 
 import java.util.List;
 
 @Config
-public class LiftSubsystem extends SubsystemBase {
+public class LiftSubsystemOther extends SubsystemBase {
 
-	List<Double> liftVelo;
-	Double targetPosition;
+	List<Double> liftVelo, liftPos;
 	MotorGroup lift;
 
 	//Define Whatever These Mean
-	public static final double kS = 0, kG = 0, kV = 0, kA = 0;
-	public static final double kP = 0, kI = 0, kD = 0, kF = 0;
-	public static final double velocitySetpoint = 10, accelSetpoint = 20;
+	public static final double kS = 0.0, kG = 0.0, kV = 0.0, kA = 0.0;
+	public static final double kP = 0.0, kI = 0.0, kD = 0.0, kF = 0.0;
+	public static final double tolerance = 10.0, velo = 10.0, accel = 20.0;
 
 	// Create a new ElevatorFeedforward
 	ElevatorFeedforward feedforward = new ElevatorFeedforward(kS, kG, kV, kA);
 	PIDFController      pidf        = new PIDFController(kP, kI, kD, kF);
-	double ffOut, pidfOut;
+	double              ffOut, pidfOut;
 
-	public LiftSubsystem(MotorGroup lift) {
+	public LiftSubsystemOther(MotorGroup lift) {
 		this.lift = lift;
-		liftVelo = lift.getVelocities();
+		liftVelo  = lift.getVelocities();
+		liftPos   = lift.getPositions();
 	}
 
-	//Froot Loop
-	@Override
-	public void periodic() {
+	public double calculate(double velocitySetpoint, double accelSetpoint, double targetPosition) {
 		ffOut = feedforward.calculate(velocitySetpoint, accelSetpoint);
 		pidf.setF(ffOut);
 		pidfOut = pidf.calculate(liftVelo.get(0), targetPosition);
+		return pidfOut;
 	}
 
-	public void liftSet (double targetPosition) {
-		this.targetPosition = targetPosition;
+	public void set(double targetPosition) {
+		double lowPos = targetPosition - tolerance, highPos = targetPosition + tolerance;
+		while (liftPos.get(0) >= lowPos && liftPos.get(0) <= highPos) {
+			calculate(velo, accel, targetPosition);
+		}
 	}
 
-	public void liftStop (){
+	public void stop() {
 		lift.set(0);
 		lift.stopMotor();
 	}
