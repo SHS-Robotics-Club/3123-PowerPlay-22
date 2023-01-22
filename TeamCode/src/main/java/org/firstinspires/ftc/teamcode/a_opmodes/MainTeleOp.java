9339d3f76364b7245a3e3bb1e13e21ad94415a2b
codyh <neochaotics@pm.me>
2023-01-22 13:34:06 -0500

    pid testing 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/a_opmodes/MainTeleOp.java
@@ -14,58 +14,61 @@ import org.firstinspires.ftc.teamcode.b_commands.DriveCommand;
 import org.firstinspires.ftc.teamcode.c_subsystems.ClawSubsystem;
 import org.firstinspires.ftc.teamcode.c_subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.c_subsystems.LiftSubsystemOther;
+import org.firstinspires.ftc.teamcode.c_subsystems.LiftSubsystemOtherOther;
 
 //@Disabled
 @Config
 @TeleOp(name = "MainTeleOp", group = ".")
 public class MainTeleOp extends CommandOpMode {
-    public static double target = 300;
+	public static double target = 300;
 
-    @Override
-    public void initialize() {
-        if (isStopRequested()) return; // Exit program if stop requested??
+	@Override
+	public void initialize() {
+		if (isStopRequested()) return; // Exit program if stop requested??
 
-        // Get Devices
-        final Devices devices = new Devices(hardwareMap);
+		// Get Devices
+		final Devices devices = new Devices(hardwareMap);
 
-        // Gamepad
-        GamepadEx gPad1 = new GamepadEx(gamepad1);
+		// Gamepad
+		GamepadEx gPad1 = new GamepadEx(gamepad1);
 
-        // Define Systems ----------------------------------------------------------------------------------------------------
-        DriveSubsystem driveSubsystem = new DriveSubsystem(devices.frontLeft, devices.frontRight, devices.backLeft, devices.backRight);
-        ClawSubsystem claw = new ClawSubsystem(devices.clawLeft, devices.clawRight);
-        LiftSubsystemOther lfo = new LiftSubsystemOther(devices.lift);
+		// Define Systems ----------------------------------------------------------------------------------------------------
+		DriveSubsystem          driveSubsystem = new DriveSubsystem(devices.frontLeft, devices.frontRight, devices.backLeft, devices.backRight);
+		ClawSubsystem           claw           = new ClawSubsystem(devices.clawLeft, devices.clawRight);
+		LiftSubsystemOtherOther lfoo           = new LiftSubsystemOtherOther(devices.lift);
 
-        DriveCommand driveCommand = new DriveCommand(driveSubsystem, gPad1::getLeftX, gPad1::getLeftY, gPad1::getRightX, 1);
+		DriveCommand driveCommand = new DriveCommand(driveSubsystem, gPad1::getLeftX, gPad1::getLeftY, gPad1::getRightX, 1);
 
-        // CONTROLS ----------------------------------------------------------------------------------------------------
-        // X Button = Claw Open/Close
-        gPad1.getGamepadButton(GamepadKeys.Button.X)
-                .whenPressed(new ConditionalCommand(
-                        new InstantCommand(claw::open, claw),
-                        new InstantCommand(claw::close, claw),
-                        () -> {
-                            claw.toggle();
-                            return claw.isOpen();
-                        }
-                ));
+		register(driveSubsystem, claw, lfoo);
 
-        gPad1.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(new RunCommand(() -> {
-            lfo.set(target);
-        }));
+		// CONTROLS ----------------------------------------------------------------------------------------------------
+		// X Button = Claw Open/Close
+		gPad1.getGamepadButton(GamepadKeys.Button.X)
+		     .whenPressed(new ConditionalCommand(
+				     new InstantCommand(claw::open, claw),
+				     new InstantCommand(claw::close, claw),
+				     () -> {
+					     claw.toggle();
+					     return claw.isOpen();
+				     }
+		     ));
 
-		gPad1.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whenPressed(new RunCommand(lfo::stop));
+		gPad1.getGamepadButton(GamepadKeys.Button.DPAD_UP).whenPressed(new RunCommand(() -> {
+			lfoo.set(target);
+		}));
+
+		gPad1.getGamepadButton(GamepadKeys.Button.DPAD_DOWN).whenPressed(new RunCommand(lfoo::stop));
 
 
-        // Register and Schedule ----------------------------------------------------------------------------------------------------
-        register(driveSubsystem);
-        schedule(driveCommand,
-                new RunCommand(() -> {
-                    // Telemetry
-                    telemetry.update();
-                    telemetry.addData("velo", devices.lift.getVelocities());
-                    telemetry.addData("pos", devices.lift.getPositions());
-					telemetry.addData("calc", lfo.calculate(10, 20, target));
-                }));
-    }
+		// Register and Schedule ----------------------------------------------------------------------------------------------------
+		schedule(driveCommand,
+		         new RunCommand(() -> {
+			         // Telemetry
+			         telemetry.update();
+			         telemetry.addData("velo", devices.lift.getVelocities());
+			         telemetry.addData("pos", devices.lift.getPositions());
+			         telemetry.addData("calc", lfoo.calculate(target));
+			         telemetry.addData("dpadup", gPad1.getGamepadButton(GamepadKeys.Button.DPAD_UP));
+		         }));
+	}
 }
